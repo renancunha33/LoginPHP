a89318053c425bce0af17801bb13ed7d59d4b039
Renan Cunha <renan.cunha20@gmail.com>
2016-04-24 14:13:37 -0300

    proteção contra erros

--- login.php
+++ login.php
@@ -22,11 +22,11 @@
       <input type="text" id="nome" name="nome"class="form-control" placeholder="Nome" required="" autofocus="">
       
       <input type="password" id="senha" name="senha"class="form-control" placeholder="Senha" required="">
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      <button class="btn btn-lg btn-primary btn-block" id="CLogin"type="submit">Entrar</button>
 
     </form>
     <form class="form-signin" action="cadastrar.html">
-     <button class="btn btn-lg btn-primary btn-block" type="submit">Cadastrar</button>
+     <button class="btn btn-lg btn-primary btn-block" id="CCadastro"type="submit">Cadastrar</button>
    </form>
    <form class="form-signin" action="banco.php">
     <button class="btn btn-lg btn-primary btn-block" id="CBanco" type="submit">Criar BANCO</button>
@@ -35,7 +35,7 @@
 </div> 
 <?php
 $oi =  "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-echo $oi == "http://localhost/login/login.php?valor=1" ? "<script>javascript:document.getElementById('CBanco').disabled = true;</script>" : 0;
+echo $oi == "http://localhost/login/login.php?valor=1" ? "<script>javascript:document.getElementById('CBanco').disabled = true;</script>" : "<script>javascript:document.getElementById('CLogin').disabled = true;</script><script>javascript:document.getElementById('CCadastro').disabled = true;</script>";
 ?>
 
 <script src="./bootstrap/ie10-viewport-bug-workaround.js">
